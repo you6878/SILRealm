eb79cadfca95aa0271c62aac0f1540f94f3f2422
유명식 <myeongsic@MacBook-Pro.local>
2018-04-10 21:31:18 +0900

    init

--- HowlRealm/ViewController.swift
+++ HowlRealm/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import RealmSwift
+class Cart :Object{
+    @objc dynamic var name = ""
+}
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+    
+    @IBOutlet weak var textfield: UITextField!
+    @IBAction func button(_ sender: Any) {
+        
+        let cart = Cart()
+        cart.name = textfield.text!
+        try! realm?.write {
+            realm?.add(cart)
+        }
+    }
+    @IBOutlet weak var tableview: UITableView!
+    var notificationToken : NotificationToken?
+    var realm:Realm?
+    var items:Results<Cart>?
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (items?.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = items![indexPath.row].name
+        
+        return cell
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Migration 이 필요할때 데이터베이스 초기화
+        let config = Realm.Configuration( deleteRealmIfMigrationNeeded: true)
+        Realm.Configuration.defaultConfiguration = config
+        realm = try! Realm()
+        
+        //Cart 데이터들을 수집하는 코드 입니다.
+        items = realm?.objects(Cart.self)
+        
+        //Push Driven 작동 시키는 코드
+        notificationToken = realm?.observe({ (noti, realm) in
+            self.tableview.reloadData()
+        })
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        notificationToken?.invalidate()
     }
 
     override func didReceiveMemoryWarning() {
